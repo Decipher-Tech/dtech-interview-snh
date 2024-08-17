fe908b2959b592836e2ad55cd11eaf864420e2a1
Md Rakin Sarder <tunchunairarko@gmail.com>
2024-08-17 17:29:59 +0100

    feat: Add lazy loading for images and update Cards component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,27 +1,46 @@
 import Header from "./header";
 import Cards from "./components/Cards";
 import OtherLanguageCards from "./components/OtherLanguageCards";
-import { useLocale } from 'next-intl';
+// import { useLocale } from 'next-intl';
 import Loader from "./components/Loader";
+import fetchPostData from "./fetch_data";
 
-export default function Home() {
-  const locale = useLocale();
-  const isEnglish = locale === 'en';
-  const isLoading = locale === ''; 
+export default async function Home() {
+  // const locale = useLocale();
+  const locale = "en";
+  const isEnglish = locale === "en";
+  // const isLoading = locale === '';
+
+  const data = await fetchPostData();
+  console.log(data);
+  if (!data) return null;
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-10 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">
       {/* Header */}
       <Header />
-
-      {/* Content */}
+      <section className="flex flex-col items-center justify-center w-full max-w-4xl text-gray-800 p-8 mt-10">
+        {/* {isEnglish ? (
+          <div className="english w-full">
+            <Cards data={data} />
+          </div>
+        ) : (
+          <div className="other-lang w-full">
+            <OtherLanguageCards />
+          </div>
+        )} */}
+        <div className="english w-full">
+          <Cards data={data} />
+        </div>
+      </section>
+      {/* Content
       {isLoading ? (
         <Loader />
       ) : (
         <section className="flex flex-col items-center justify-center w-full max-w-4xl text-gray-800 p-8 mt-10">
           {isEnglish ? (
             <div className="english w-full">
-              <Cards />
+              <Cards data={data}/>
             </div>
           ) : (
             <div className="other-lang w-full">
@@ -29,7 +48,7 @@ export default function Home() {
             </div>
           )}
         </section>
-      )}
+      )} */}
     </main>
   );
 }
